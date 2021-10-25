7b21e17c2bf69e9adc30575b06d90cc0f6b7607a
kirti garg <kirti.garg@innovaccer.com>
2021-10-25 13:25:28 +0530

    test file

---
+++ test_cuboidv.py
@@ -0,0 +1,10 @@
+from cuboid_volume import *
+import unittest
+class TestCuboid(unittest.TestCase):
+    def test_volume(self):
+        self.assertAlmostEqual(cuboid_volume(2),8)
+        self.assertAlmostEqual(cuboid_volume(1),1)
+        self.assertAlmostEqual(cuboid_volume(0),0)
+        self.assertAlmostEqual(cuboid_volume(5.5),166.375)
+if __name__=='__main__':      
+    unittest.main()
